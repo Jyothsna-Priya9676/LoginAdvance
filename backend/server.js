84f33d628ab3d16f00bc915849daa43be151e56c
Jyothsna-Priya9676 <jpriya9676@gmail.com>
2026-07-16 19:18:42 +0530

    Fix email sending timeout and OTP registration flow

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,5 @@
-// const express=require("express")
-// const dotenv=require("dotenv");
-// const mongoose=require("mongoose");
-// const connectDB=require('./config/db.js');
-// const cors=require("cors");
-// const cookieParser=require("cookie-parser");
-
-// dotenv.config();
-// connectDB();
-
-// const app=express();
-// app.use(express.json());
-// app.use(cors());
-// app.use("./api/auth",authRoutes);
-// const PORT=process.env.PORT||5000;
-// app.listen(PORT,()=>{
-//     console.log(`server Runnig on ${PORT}`);
-// })
-
-
-
 const dotenv = require("dotenv");
-
+dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -28,7 +7,7 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 
-dotenv.config();
+
 
 connectDB();
 
