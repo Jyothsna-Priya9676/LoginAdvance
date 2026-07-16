c2523374db4074c37f414121f4b57d6c21bb0d05
Jyothsna-Priya9676 <jpriya9676@gmail.com>
2026-07-16 15:00:37 +0530

    Add register error logging

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,13 +5,11 @@ const sendEmail = require("../utils/sendEmail");
 exports.register = async (req, res) => {
     try {
         console.log("Request Body:", req.body);
-
-        const { name, email, password } = req.body;
-
-        console.log(name, email, password);
+       
 
         // rest of your code...
         const { name, email, password } = req.body;
+         console.log(name, email, password);
 
         const existingUser = await User.findOne({ email });
 
