7786ada21fb7623bb48269a29141672ee7fa9f36
Jyothsna-Priya9676 <jpriya9676@gmail.com>
2026-07-16 14:58:20 +0530

    Add register error logging

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,68 +1,9 @@
-// const nodemailer=require("nodemailer");
-// const sendEmail=async(to,subject,text)=>{
-//     try{
-//         const transporter=nodemailer.createTransport({
-//             service:"gmail",
-//             auth:{
-//                 user:process.env.EMAIL,
-//                 pass:process.env.APP_PASSWORD
-//             }
-//         });
-//         const mailOptions={
-//             from:process.env.EMAIL,
-//             to,subject,text
-//         }
-//         await transporter.sendMail(mailOptions);
-//         console.log("Email sent successfully");
-//     }
-//     catch(err){
-//         console.log(err);
-//     }
-// }
-// module.exports=sendEmail
-
-
-
-
-
-// const nodemailer = require("nodemailer");
-
-// const sendEmail = async (to, subject, text) => {
-//     try {
-//         const transporter = nodemailer.createTransport({
-//             service: "gmail",
-//             auth: {
-//                 user: process.env.EMAIL,
-//                 pass: process.env.APP_PASSWORD,
-//             },
-//         });
-
-//         const mailOptions = {
-//             from: process.env.EMAIL,
-//             to,
-//             subject,
-//             text,
-//         };
-
-//         await transporter.sendMail(mailOptions);
-
-//         console.log("Email sent successfully");
-//     } catch (error) {
-//         console.log("Email Error:", error);
-//         throw error;
-//     }
-// };
-
-// module.exports = sendEmail;
-
-
-
-
-
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (to, subject, text) => {
     try {
+        console.log("EMAIL:", process.env.EMAIL);
+console.log("APP_PASSWORD exists:", !!process.env.APP_PASSWORD);
 
         const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -92,7 +33,9 @@ const sendEmail = async (to, subject, text) => {
 
     } catch (error) {
 
-        console.log("Email Error:", error);
+        console.error("Email Error:", error);
+console.error("Message:", error.message);
+console.error("Code:", error.code);
         throw error;
 
     }
